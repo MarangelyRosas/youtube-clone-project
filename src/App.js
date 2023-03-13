088ada7b461f7c397200fccee24cecfbcdec28a0
Marangely Rosas <Ladykaliber22@gmail.com>
2023-03-13 17:34:25 -0400

    Started creating Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import "./App.css";
 import Nav from "./components/common/Nav.js";
+//import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//import Home from "./components/Home.js";
+import Footer from "./components/common/Footer.js";
 
 
+//const apiURL =  `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
 function App() {
+ 
+
   return (
-    <div>
-      <Nav />
+    <div className="App">
+      <Router>
+        <Nav />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="/videos" element={<ShowsVideo />} /> */}
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
+
 export default App;
--- src/api/fetch.js
+++
@@ -1,8 +0,0 @@
-// Videos
-const apiURL =  `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
-
-// Index/Get all Videos
- export function getAllVideos() {
-    return fetch(`${apiURL}/videos`)
-    .then(response => response.json()); // fetch returns an array of videos in JS.
-  }
